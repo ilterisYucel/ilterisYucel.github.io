ba4733afc995b5f3284ce7197e21137bdb7943ee
ilteris yucel <ilteris.yucel@hacettepe.edu.tr>
2023-04-09 15:20:21 +0300

    Structure

---
+++ app.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+var main = document.getElementById("main");
+main.innerHTML = "<h1>Selamın Aleyküm Bilal Emmi</h1>";
+/******/ })()
+;
